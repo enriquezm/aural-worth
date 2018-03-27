95aa4a61d9a32e3a6076526bfa07c46df273dc81
Myles Enriquez <enri.myles@gmail.com>
2018-03-26 23:09:08 -0700

    Add email/phone.

--- footer.php
+++ footer.php
@@ -14,12 +14,12 @@
 				<!-- Footer -->
 				<footer id="footer">
 					<ul class="icons">
-						<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
 						<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
 						<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
 						<li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
-						<li><a href="#" class="icon alt fa-phone"><span class="label">Phone</span></a></li>
-						<li><a href="#" class="icon alt fa-envelope-o"><span class="label">Email</span></a></li>
+						<li><a href="#" class="fa fa-phone"><span class="label">Phone</span></a></li>
+						<li><a href="#" class="fa fa-envelope"><span class="label">Email</span></a></li>
 					</ul>
 					<p class="copyright">&copy; Auralworth Acoustics, LLC. All rights reserved.</p>
 				</footer>
