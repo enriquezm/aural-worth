7a58f8f48c06e8e1beba452ceeffae2fb0c1ca09
Myles Enriquez <enri.myles@gmail.com>
2018-03-29 21:45:40 -0700

    Update site title.

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 -->
 <html>
 	<head>
-		<title>Untitled</title>
+		<title><?php get_bloginfo('name'); ?></title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<?php wp_head() ?>
