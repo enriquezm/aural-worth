0f5a35b300c3a8c774e4ee97151df1b0ece9f8cb
Myles Enriquez <enri.myles@gmail.com>
2018-04-21 22:48:39 -0700

    Rename posts to services.

--- functions.php
+++ functions.php
@@ -7,7 +7,9 @@
  * @package AuralWorth
  */
 
-/* Enqueue styles and scripts */
+/**
+ * Enqueue styles and scripts
+ */
 function aural_worth_enqueue_assets() {
   /* theme's primary style.css file (root) */
   wp_enqueue_style( 'main-css' , get_stylesheet_uri() );
@@ -25,10 +27,31 @@ function aural_worth_enqueue_assets() {
   wp_enqueue_script( 'util' , get_template_directory_uri() . '/assets/js/util.js' );
   wp_enqueue_script( 'aural-worth-js' , get_template_directory_uri() . '/assets/js/main.js' );
 }
-add_action( 'wp_enqueue_scripts' , 'aural_worth_enqueue_assets' );
-
-// Add thumbnail support for posts
-add_theme_support( 'post-thumbnails' );
+function revcon_change_post_label() {
+    global $menu;
+    global $submenu;
+    $menu[5][0] = 'Services';
+    $submenu['edit.php'][5][0] = 'Services';
+    $submenu['edit.php'][10][0] = 'Add Service';
+    $submenu['edit.php'][16][0] = 'Service Tags';
+}
+function revcon_change_post_object() {
+    global $wp_post_types;
+    $labels = &$wp_post_types['post']->labels;
+    $labels->name = 'Services';
+    $labels->singular_name = 'Service';
+    $labels->add_new = 'Add Service';
+    $labels->add_new_item = 'Add Service';
+    $labels->edit_item = 'Edit Service';
+    $labels->new_item = 'Service';
+    $labels->view_item = 'View Service';
+    $labels->search_items = 'Search Service';
+    $labels->not_found = 'No Service found';
+    $labels->not_found_in_trash = 'No Service found in Trash';
+    $labels->all_items = 'All Service';
+    $labels->menu_name = 'Service';
+    $labels->name_admin_bar = 'Service';
+}
 
 /**
  * Custom Post Type Creation
@@ -39,7 +62,7 @@ function create_posttypes() {
     // CPT Options
     array(
       'labels' => array(
-          'name' => __( 'Team Members' ),
+          'name' => __( 'Members' ),
           'singular_name' => __( 'Member' ),
           'description' => __( 'People who work on the AuralWorth Acoustics team.' )
       ),
@@ -76,5 +99,14 @@ function create_posttypes() {
     )
   );
 }
-// Hooking up our function to theme setup
+
+// Theme setup
 add_action( 'init', 'create_posttypes' );
+add_action( 'wp_enqueue_scripts' , 'aural_worth_enqueue_assets' );
+add_action( 'admin_menu', 'revcon_change_post_label' );
+add_action( 'init', 'revcon_change_post_object' );
+
+// Theme support
+add_theme_support( 'post-thumbnails' );
+
+//
