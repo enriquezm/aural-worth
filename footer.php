3df16e5866ccdaf141d088c5e3d96838eafca5db
Myles Enriquez <enri.myles@gmail.com>
2018-05-28 08:25:41 -0700

    Add social media.

--- footer.php
+++ footer.php
@@ -14,12 +14,10 @@
 				<!-- Footer -->
 				<footer id="footer">
 					<ul class="icons">
-						<!-- <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
-						<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li> -->
-						<!-- <li><a href="#" class="fa fa-phone"><span class="label">Phone</span></a></li>
-						<li><a href="#" class="fa fa-envelope"><span class="label">Email</span></a></li> -->
+						<li><a href="https://twitter.com/auralworth" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="https://www.facebook.com/auralworth/" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="https://www.instagram.com/auralworth/" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="https://www.linkedin.com/company/auralworth/" class="icon alt fa-linkedin"><span class="label">Linkedin</span></a></li>
 					</ul>
 					<p class="copyright">&copy; Auralworth Acoustics LLC. All rights reserved. <a href="tel:+17027026616">702.706.6616</a></p>
 				</footer>
